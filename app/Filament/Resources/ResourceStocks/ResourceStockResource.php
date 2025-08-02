3bc962afe330d56d1d21ffb1eb3f99fc1145b6e1
Mansoor Khan <8mansoorkhan@gmail.com>
2025-08-02 15:00:39 +0500

    wip

--- app/Filament/Resources/ResourceStocks/ResourceStockResource.php
+++ app/Filament/Resources/ResourceStocks/ResourceStockResource.php
@@ -18,7 +18,7 @@ class ResourceStockResource extends Resource
 {
     protected static ?string $model = ResourceStock::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBuildingStorefront;
 
     public static function form(Schema $schema): Schema
     {
