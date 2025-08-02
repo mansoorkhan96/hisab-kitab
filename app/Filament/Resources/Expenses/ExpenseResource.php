2a0ae169cf96db75cba849cdb3a6af8db93ce487
Mansoor Khan <8mansoorkhan@gmail.com>
2025-08-02 15:31:15 +0500

    wip

--- app/Filament/Resources/Expenses/ExpenseResource.php
+++ app/Filament/Resources/Expenses/ExpenseResource.php
@@ -18,7 +18,7 @@ class ExpenseResource extends Resource
 {
     protected static ?string $model = Expense::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBanknotes;
 
     public static function form(Schema $schema): Schema
     {
