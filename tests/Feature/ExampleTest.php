4d0c160fca2d0f3c816a84420f9797cdb7df37c3
Mansoor Khan <beingdeveloper96@gmail.com>
2026-09-21 20:27:18 +0500

    assert guests redirect to filament login

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,11 @@
 <?php
 
-test('the application returns a successful response', function () {
-    $response = $this->get('/');
+test('guests are redirected to the login page', function () {
+    $this->get('/')
+        ->assertRedirectToRoute('filament.admin.auth.login');
+});
 
-    $response->assertStatus(200);
+test('the login page returns a successful response', function () {
+    $this->get(route('filament.admin.auth.login'))
+        ->assertOk();
 });
